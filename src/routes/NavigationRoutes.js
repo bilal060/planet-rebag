ccad9459850359a9fb8d946814aeac2d733d04d4
muhammadbilal <ahmad_4al@hotmail.com>
2023-08-30 11:30:16 +0500

    latest code

--- src/routes/NavigationRoutes.js
+++ src/routes/NavigationRoutes.js
@@ -1,29 +1,28 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import MainLayout from "../layout/MainLayout.js";
-import AuthLayout from "../layout/AuthLayout.js";
-import Stores from "../pages/Stores.js";
-import Requests from "../pages/Requests.js";
-
-import Stores from "../pages/Stores.js";
-const AccountPrivacyPolicy =React.lazy(()=> import("../components/accountSetting/PrivacyPolicy.js")) ;
-const AccountFaqs =React.lazy(()=> import("../components/accountSetting/Faqs.js")) ;
-const ContactUs =React.lazy(()=> import("../components/accountSetting/ContactUs.js")) ;
-const AccountNewPassword =React.lazy(()=> import("../components/accountSetting/ChangePassword.js")) ;
-const AccountSettingSidebar =React.lazy(()=> import("../components/accountSetting/Sidebar.js")) ;
-const TransactionHistory = React.lazy(() => import("../pages/TransactionHistory.js"));
-
+const MainLayout = React.lazy(() => import("../layout/MainLayout.js"));
+const Stores = React.lazy(() => import("../pages/Stores.js"));
+const Requests = React.lazy(() => import("../pages/Requests.js"));
+const AccountNewPassword = React.lazy(() =>
+  import("../components/accountSetting/ChangePassword.js"),
+);
+const AccountSetting = React.lazy(() =>
+  import("../layout/AccountSettingLayout.js"),
+);
+const TransactionHistory = React.lazy(() =>
+  import("../pages/TransactionHistory.js"),
+);
 const StoreDetail = React.lazy(() => import("../pages/StoreDetail.js"));
 const AppUserCardView = React.lazy(() => import("../pages/AppUserCardView.js"));
 const AppUserDetailView = React.lazy(() =>
-  import("../pages/AppUserDetailView.js")
+  import("../pages/AppUserDetailView.js"),
 );
 const ItemCategories = React.lazy(() => import("../pages/ItemCategories.js"));
 const Home = React.lazy(() => import("../pages/Home.js"));
 const Landing = React.lazy(() => import("../pages/Landing.js"));
 const CheckMail = React.lazy(() => import("../pages/CheckMail.js"));
 const ForgetPassword = React.lazy(() => import("../pages/ForgetPassword.js"));
-const TempPassword = React.lazy(() => import("../pages/TempPassword.js"));
+const Signup = React.lazy(() => import("../pages/Signup.js"));
 const Login = React.lazy(() => import("../pages/Login.js"));
 const SubstoreLogin = React.lazy(() => import("../pages/SubstoreLogin.js"));
 const OtpMobile = React.lazy(() => import("../pages/OtpMobile.js"));
@@ -37,91 +36,90 @@ function NavigationRoutes() {
       <Route
         exact
         path="/home"
-        element={<Home />}
+        element={
+          <MainLayout>
+            <Home />
+          </MainLayout>
+        }
       ></Route>
       <Route
         exact
         path="/stores"
-        element={<Stores />}
+        element={
+          <MainLayout>
+            <Stores />
+          </MainLayout>
+        }
       ></Route>
       <Route
         exact
         path="/stores-details"
-        element={<StoreDetail />}
+        element={
+          <MainLayout>
+            <StoreDetail />
+          </MainLayout>
+        }
       ></Route>
       <Route
         exact
         path="/users"
-        element={<AppUserCardView />}
+        element={
+          <MainLayout>
+            <AppUserCardView />
+          </MainLayout>
+        }
       ></Route>
       <Route
         exact
         path="/users/detail"
-        element={<AppUserDetailView />}
+        element={
+          <MainLayout>
+            <AppUserDetailView />
+          </MainLayout>
+        }
       ></Route>
       <Route
         exact
         path="/item-categories"
-        element={<ItemCategories />}
+        element={
+          <MainLayout>
+            <ItemCategories />
+          </MainLayout>
+        }
       ></Route>
       <Route
         exact
         path="/transaction-history"
-        element={<TransactionHistory />}
+        element={
+          <MainLayout>
+            <TransactionHistory />
+          </MainLayout>
+        }
       ></Route>
       <Route
         exact
         path="/settings/ChangePassword"
         element={
-               <AccountNewPassword/>        }
-      ></Route>
-       <Route
-        exact
-        path="/settings/Contact"
-        element={
-           <AccountSetting>
-               <ContactUs/>
-            </AccountSetting>
-        }
-      ></Route>
-       <Route
-        exact
-        path="/settings/Faqs"
-        element={
-           <AccountSetting>
-               <AccountFaqs/>
-            </AccountSetting>
-        }
-      ></Route>
-       <Route
-        exact
-        path="/settings/PrivacyPolicy"
-        element={
-           <AccountSetting>
-               <AccountPrivacyPolicy/>
-            </AccountSetting>
+          <AccountSetting>
+            <AccountNewPassword />
+          </AccountSetting>
         }
       ></Route>
       <Route
         exact
-        path="/temppassword"
+        path="/requests"
         element={
-          <AuthLayout>
-            <TempPassword />
-          </AuthLayout>
+          <MainLayout>
+            <Requests />
+          </MainLayout>
         }
       ></Route>
-      <Route exact path="/Settings" element={ <AccountSetting>
-               <AccountNewPassword/>
-            </AccountSetting>}></Route>
-      <Route exact path="/requests" element={<></>}></Route>
-      <Route exact path="/support" element={<></>}></Route>
-
+      <Route exact path="/support" element={<MainLayout></MainLayout>}></Route>
       <Route exact path="/" element={<Landing />}></Route>
 
       <Route exact path="/checkMail" element={<CheckMail />}></Route>
       <Route exact path="/forgetPassword" element={<ForgetPassword />}></Route>
-
+      <Route exact path="/signup" element={<Signup />}></Route>
       <Route exact path="/login" element={<Login />}></Route>
       <Route exact path="/substorelogin" element={<SubstoreLogin />}></Route>
       <Route exact path="/otpmobile" element={<OtpMobile />}></Route>
