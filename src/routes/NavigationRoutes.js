2dd2bf14947b21cf687d7c7fbbcf15ec7175686d
muhammadbilal <ahmad_4al@hotmail.com>
2023-09-04 13:05:13 +0500

    navigation modify

--- src/routes/NavigationRoutes.js
+++ src/routes/NavigationRoutes.js
@@ -4,14 +4,9 @@ import Stores from "../pages/Stores.js";
 // const AccountSettingSidebar = React.lazy(() =>
 //   import("../components/accountSetting/Sidebar.js"),
 // );
-const TransactionHistory = React.lazy(() =>
-  import("../pages/TransactionHistory.js"),
-);
+
 const StoreDetail = React.lazy(() => import("../pages/StoreDetail.js"));
 const AppUserCardView = React.lazy(() => import("../pages/AppUserCardView.js"));
-const AppUserDetailView = React.lazy(() =>
-  import("../pages/AppUserDetailView.js"),
-);
 
 const ItemCategories = React.lazy(() => import("../pages/ItemCategories.js"));
 const Requests = React.lazy(() => import("../pages/Requests.js"));
@@ -26,6 +21,27 @@ const OtpMobile = React.lazy(() => import("../pages/OtpMobile.js"));
 const OtpMail = React.lazy(() => import("../pages/OtpMail.js"));
 const Confirmation = React.lazy(() => import("../pages/Confirmation.js"));
 const Adminlogin = React.lazy(() => import("../pages/Adminlogin.js"));
+const AccountPrivacyPolicy = React.lazy(() =>
+  import("../components/accountSetting/PrivacyPolicy.js"),
+);
+const AccountFaqs = React.lazy(() =>
+  import("../components/accountSetting/Faqs.js"),
+);
+const ContactUs = React.lazy(() =>
+  import("../components/accountSetting/ContactUs.js"),
+);
+const AccountNewPassword = React.lazy(() =>
+  import("../components/accountSetting/ChangePassword.js"),
+);
+const AccountSetting = React.lazy(() =>
+  import("../layout/AccountSettingLayout.js"),
+);
+const TransactionHistory = React.lazy(() =>
+  import("../pages/TransactionHistory.js"),
+);
+const AppUserDetailView = React.lazy(() =>
+  import("../pages/AppUserDetailView.js"),
+);
 
 function NavigationRoutes() {
   const routes = (
@@ -44,7 +60,6 @@ function NavigationRoutes() {
       <Route exact path="/requests" element={<Requests />}></Route>
       <Route exact path="/support" element={<></>}></Route>
       <Route exact path="/" element={<Landing />}></Route>
-
       <Route exact path="/checkMail" element={<CheckMail />}></Route>
       <Route exact path="/forgetPassword" element={<ForgetPassword />}></Route>
       <Route exact path="/temppassword" element={<TempPasswordPage />}></Route>
@@ -54,6 +69,49 @@ function NavigationRoutes() {
       <Route exact path="/otpmail" element={<OtpMail />}></Route>
       <Route exact path="/confirmation" element={<Confirmation />}></Route>
       <Route exact path="/adminlogin" element={<Adminlogin />}></Route>
+      <Route
+        exact
+        path="/settings/ChangePassword"
+        element={
+          <AccountSetting>{/* <AccountNewPassword /> */}</AccountSetting>
+        }
+      ></Route>
+      <Route
+        exact
+        path="/settings/Contact"
+        element={
+          <AccountSetting>
+            <ContactUs />
+          </AccountSetting>
+        }
+      ></Route>
+      <Route
+        exact
+        path="/settings/Faqs"
+        element={
+          <AccountSetting>
+            <AccountFaqs />
+          </AccountSetting>
+        }
+      ></Route>
+      <Route
+        exact
+        path="/settings/PrivacyPolicy"
+        element={
+          <AccountSetting>
+            <AccountPrivacyPolicy />
+          </AccountSetting>
+        }
+      ></Route>
+      <Route
+        exact
+        path="/Settings"
+        element={
+          <AccountSetting>
+            <AccountNewPassword />
+          </AccountSetting>
+        }
+      ></Route>{" "}
     </Routes>
   );
 
