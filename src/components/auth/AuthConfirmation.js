4901e59be0df848a48688c8affe702f8a5cf3a45
Bilal060 <ahmad_4al@hotmail.com>
2023-09-04 18:53:12 +0500

    2 pages responsive

--- src/components/auth/AuthConfirmation.js
+++ src/components/auth/AuthConfirmation.js
@@ -1,28 +1,31 @@
 import React from "react";
 import congratulations_icon from "../../assets/images/congratulations.svg";
-import logo_rebag from "../../assets/images/plant-rebag-logo.svg";
-
+import Logo from "../../assets/images/Logo.svg";
+import AuthFooter from "../../components/auth_footer/AuthFooter";
 const AuthConfirmation = () => {
   return (
     <>
       <div className="login-container">
-        <div className="login-sub">
-          <div className="logo-image d-flex justify-content-center">
-            <img src={logo_rebag} alt="" className="img-fluid mb-2 logo-hide" />
-          </div>
-          <span className="mb-5">
-            <div className="logo-image d-flex justify-content-center">
-              <img src={congratulations_icon} alt="" className="logo" />
-            </div>
-            <h3 className="d-flex justify-content-center green-text fw-bold mt-3">
-              Congratulations!
-            </h3>
-            <span className="logo-text d-flex justify-content-center mt-2">
-              Your account has been created successfully.
+        <div className="login-sub text-center flex-column">
+          <img src={Logo} alt="" className="d-sm-none d-block mt-5 pt-5" />
+          <div className="auth-responsive">
+            <span className="mb-5">
+              <div className="logo-image d-flex justify-content-center">
+                <img src={congratulations_icon} alt="" className="logo" />
+              </div>
+              <h3 className="d-flex justify-content-center green-text fw-bold mt-3">
+                Congratulations!
+              </h3>
+              <span className="logo-text d-flex justify-content-center mt-2">
+                Your account has been created successfully.
+              </span>
             </span>
-          </span>
-          <div className="login-btn mt-5">
-            <button className="guest-btn btn-lg btn-block">Done</button>
+            <div className="login-btn mt-5">
+              <button className="guest-btn btn-lg btn-block">Done</button>
+            </div>
+            <div className="d-md-none d-block">
+              <AuthFooter />
+            </div>
           </div>
         </div>
       </div>
