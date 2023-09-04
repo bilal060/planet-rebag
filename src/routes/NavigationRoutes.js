2fa4b6e35b8adfd5f6dcdbd400b1f88aa7956dc8
Muhammad Mohsin <ahmad_4al@hotmail.com>
2023-09-04 15:24:38 +0500

    move componets from pages

--- src/routes/NavigationRoutes.js
+++ src/routes/NavigationRoutes.js
@@ -11,16 +11,18 @@ const AppUserCardView = React.lazy(() => import("../pages/AppUserCardView.js"));
 const ItemCategories = React.lazy(() => import("../pages/ItemCategories.js"));
 const Requests = React.lazy(() => import("../pages/Requests.js"));
 const Home = React.lazy(() => import("../pages/Home.js"));
-const Landing = React.lazy(() => import("../pages/Landing.js"));
+const LandingPage = React.lazy(() => import("../pages/Landing.js"));
 const CheckMail = React.lazy(() => import("../pages/CheckMail.js"));
-const ForgetPassword = React.lazy(() => import("../pages/ForgetPassword.js"));
+const ForgetPasswordPage = React.lazy(() =>
+  import("../pages/ForgetPassword.js"),
+);
 const TempPasswordPage = React.lazy(() => import("../pages/TempPassword.js"));
-const Login = React.lazy(() => import("../pages/Login.js"));
+const LoginPage = React.lazy(() => import("../pages/Login.js"));
 const SubstoreLogin = React.lazy(() => import("../pages/SubstoreLogin.js"));
 const OtpMobile = React.lazy(() => import("../pages/OtpMobile.js"));
 const OtpMail = React.lazy(() => import("../pages/OtpMail.js"));
 const Confirmation = React.lazy(() => import("../pages/Confirmation.js"));
-const Adminlogin = React.lazy(() => import("../pages/Adminlogin.js"));
+const AdminLoginPage = React.lazy(() => import("../pages/Adminlogin.js"));
 const AccountPrivacyPolicy = React.lazy(() =>
   import("../components/accountSetting/PrivacyPolicy.js"),
 );
@@ -56,16 +58,20 @@ function NavigationRoutes() {
       ></Route>
       <Route exact path="/requests" element={<Requests />}></Route>
       <Route exact path="/support" element={<></>}></Route>
-      <Route exact path="/" element={<Landing />}></Route>
+      <Route exact path="/" element={<LandingPage />}></Route>
       <Route exact path="/checkMail" element={<CheckMail />}></Route>
-      <Route exact path="/forgetPassword" element={<ForgetPassword />}></Route>
+      <Route
+        exact
+        path="/forgetPassword"
+        element={<ForgetPasswordPage />}
+      ></Route>
       <Route exact path="/temppassword" element={<TempPasswordPage />}></Route>
-      <Route exact path="/login" element={<Login />}></Route>
+      <Route exact path="/login" element={<LoginPage />}></Route>
       <Route exact path="/substorelogin" element={<SubstoreLogin />}></Route>
       <Route exact path="/otpmobile" element={<OtpMobile />}></Route>
       <Route exact path="/otpmail" element={<OtpMail />}></Route>
       <Route exact path="/confirmation" element={<Confirmation />}></Route>
-      <Route exact path="/adminlogin" element={<Adminlogin />}></Route>
+      <Route exact path="/adminlogin" element={<AdminLoginPage />}></Route>
       <Route
         exact
         path="/settings/ChangePassword"
