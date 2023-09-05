eae0ea35888bf28b22bbf997505f8750e0de9d6e
Muhammad Mohsin <ahmad_4al@hotmail.com>
2023-09-05 16:03:50 +0500

    Add formik on auth pages

--- src/components/auth/ForgetPassword.js
+++ src/components/auth/ForgetPassword.js
@@ -2,8 +2,19 @@ import React from "react";
 import Logo from "../../assets/images/Logo.svg";
 import "../../assets/css/auth.css";
 import AuthFooter from "../../components/auth_footer/AuthFooter";
+import { ErrorMessage, Form, Formik } from "formik";
+import * as Yup from "yup";
+import TextField from "../../shared/TextField";
+
+const forgotPasswordValidationSchema = Yup.object().shape({
+  email: Yup.string().email().required("Email is Required"),
+});
 
 function ForgetPassword() {
+  const initialValues = {
+    email: "",
+  };
+
   return (
     <>
       <div className="login-container">
@@ -15,27 +26,39 @@ function ForgetPassword() {
               Enter your associated with your account and we’ll send an email
               with instruction to reset your password.
             </span>
-            <form>
-              <div className="form-group mt-sm-5 mt-4">
-                <div className="label-inputs-start">
-                  <label htmlFor="emailInput" className="font-16 text-start">
-                    Email
-                  </label>
-                </div>
-                <div className="input-group mt-2">
-                  <input
-                    type="email"
-                    className="form-control emailInput"
-                    placeholder="@ Enter your email"
-                  />
-                </div>
-              </div>
-              <div className="login-btn mt-3">
-                <button className="guest-btn btn-lg btn-block">
-                  Create Account
-                </button>
-              </div>
-            </form>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={forgotPasswordValidationSchema}
+            >
+              {() => (
+                <Form>
+                  <div className="radio-btn mt-4"></div>
+                  <div className="form-group mb-3">
+                    <div className="label-inputs-start mb-2">
+                      <label htmlFor="emailInput" className="font-16">
+                        Email
+                      </label>
+                    </div>
+                    <TextField
+                      placeholder="@ Enter your email"
+                      name="email"
+                      type={"email"}
+                    />
+                    <ErrorMessage
+                      component="div"
+                      name="email"
+                      className="invalid-feedback"
+                    />
+                  </div>
+
+                  <div className="login-btn mt-3">
+                    <button className="guest-btn btn-lg btn-block mt-2">
+                      Reset Password
+                    </button>
+                  </div>
+                </Form>
+              )}
+            </Formik>
             <div className="d-md-none d-block">
               <AuthFooter />
             </div>
