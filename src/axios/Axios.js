01493076f68448cfc8bbac6ae29696d193d7fd68
bilal060 <ahmad_4al@hotmail.com>
2023-10-10 22:59:46 +0500

    Loading and axios issues

--- src/axios/Axios.js
+++ src/axios/Axios.js
@@ -4,4 +4,21 @@ const Axios = axios.create({
   baseURL: `${process.env.REACT_APP_SERVER_URL}/api/`,
 });
 
+Axios.interceptors.request.use((request) => {
+  document.querySelector(".loading-section").style.display = "flex";
+  return request;
+});
+
+Axios.interceptors.response.use(
+  (response) => {
+    document.querySelector(".loading-section").style.display = "none";
+
+    return response;
+  },
+  (error) => {
+    document.querySelector(".loading-section").style.display = "none";
+    throw error;
+  },
+);
+
 export default Axios;
