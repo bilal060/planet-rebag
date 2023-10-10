92426be0f4179a328d80795e11e48478979c24f9
Muhammad Bilal <bilal.xbt@gmail.com>
2023-10-10 18:23:21 +0500

    changes

--- src/store/user/actions/actionCreators.js
+++ src/store/user/actions/actionCreators.js
@@ -8,23 +8,51 @@ import Axios from "../../../axios/Axios";
 // };
 
 export const userLogin = (data, navigation) => (dispatch) => {
-  Axios.post("users/login", data)
+  Axios.post("user/login", data)
     .then((response) => {
       dispatch({
         type: actionTypes.USER_LOGIN,
         payload: response.data,
       });
-      navigation("/");
+      navigation("/home");
       Toast.success(response.data.status);
     })
     .catch((error) => {
       Toast.error(error.response.data.message);
     });
+  console.log(data);
 };
 
-// export const userLogin = () => (dispatch) => {
-//   dispatch({
-//     type: actionTypes.USER_LOGIN,
-//     payload: "Login working",
-//   });
-// };
+export const userSignup =
+  (data, selectedLoginOption, navigation) => (dispatch) => {
+    const registrationData = {
+      role: "user",
+      name: data.name,
+      email: data.email,
+      phone: data.phone,
+      password: data.password,
+    };
+    const signupEndpoint =
+      selectedLoginOption === "email"
+        ? "user/signup/email"
+        : "user/signup/mobile";
+
+    Axios.post(signupEndpoint, registrationData)
+      .then((response) => {
+        dispatch({
+          type: actionTypes.USER_SIGNUP,
+          payload: response.data,
+        });
+        if (selectedLoginOption === "email") {
+          navigation("/otpmail");
+        } else if (selectedLoginOption === "mobile") {
+          navigation("/otpmobile");
+        }
+
+        Toast.success(response.data.status);
+      })
+      .catch((error) => {
+        console.error("Signup Error", error);
+        Toast.error(error.response.data.message);
+      });
+  };
