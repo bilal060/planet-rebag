79725edb18d2f90cf3f6ef199f2fbb61e4907675
Bilal <mbilal.dev001@gmail.com>
2023-10-17 14:03:40 +0500

    refactor store

--- src/pages/Stores.js
+++ src/pages/Stores.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../assets/css/stores.css";
-import Store from "../components/Store";
+import Store from "../components/store/Store";
 import withMainLayout from "../layout/MainLayout";
 const Stores = () => {
   return (
