7f4bb365960fd3e8f6bd999ebb8436ab9c43b29b
Bilal <mbilal.dev001@gmail.com>
2023-10-18 10:49:18 +0500

    fix store

--- src/components/store/viewStores.js
+++ src/components/store/viewStores.js
@@ -8,23 +8,34 @@ import PropTypes from "prop-types";
 const ViewStores = ({ stores }) => {
   return (
     <Row className="m-0 stores-cards">
-      {stores?.map((data, index) => {
-        return (
-          <Col xl="6" className="mb-4 col" key={index}>
-            <StoresCard
-              storeName={data.storeName}
-              storeId={data.storeId}
-              itemsReturned={data.itemsReturned}
-              totalRedeemPrice={data.totalRedeemPrice}
-              totalSubStores={data.totalSubStores}
-              numberOfPos={data.numberOfPos}
-              storeLocation={data.storeLocation}
-              storeIcon={<StoreLogo />}
-              btnText="View Store"
-            />
-          </Col>
-        );
-      })}
+      {stores && stores?.length === 0 ? (
+        <Col xl="12" className="mb-4 col">
+          <div className="d-flex justify-content-center align-content-center border border-lg p-5">
+            <h4 className="fs-3 ">No Stores Available</h4>
+          </div>
+        </Col>
+      ) : (
+        <>
+          {stores?.map((data, index) => {
+            return (
+              <Col xl="6" className="mb-4 col" key={index}>
+                <StoresCard
+                  storeName={data.storeName}
+                  storeId={data.storeId}
+                  itemsReturned={data.itemsReturned}
+                  totalRedeemPrice={data.totalRedeemPrice}
+                  totalSubStores={data.totalSubStores}
+                  numberOfPos={data.numberOfPos}
+                  storeLocation={data.storeLocation}
+                  storeIcon={<StoreLogo />}
+                  btnText="View Store"
+                  storeType={data.storeType}
+                />
+              </Col>
+            );
+          })}
+        </>
+      )}
     </Row>
   );
 };
