24bcf7810ff6d06df0befea1888a3ce998244e32
Bilal <mbilal.dev001@gmail.com>
2023-10-18 11:25:41 +0500

    add store

---
+++ src/shared/ImageDisplay.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+
+const ImageDisplay = (props) => {
+  const [imageError, setImageError] = useState(false);
+  const [image, setImage] = useState("");
+  const handleImageError = () => {
+    setImageError(true);
+  };
+
+  useEffect(() => {
+    if (props?.src) {
+      setImage(props?.src);
+      setImageError(false);
+    }
+  }, [props?.src]);
+
+  return (
+    <>
+      {imageError ? (
+        // Render a placeholder image when there is an error loading the image
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSZpPDnjQBynSxEHYXo-orWYtvNybtifvhMUdsVjEhuOFpkDYPshISj_TpITVzJullxW4&usqp=CAU"
+          alt="Placeholder"
+          className={props?.className || ""}
+          style={props?.style || {}}
+        />
+      ) : (
+        // Render the actual image, and attach the error handler
+        <img
+          src={image}
+          alt={props?.alt}
+          className={props?.className || ""}
+          style={props?.style || {}}
+          onError={handleImageError}
+        />
+      )}
+    </>
+  );
+};
+
+ImageDisplay.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  className: PropTypes.string,
+  style: PropTypes.object,
+};
+
+export default ImageDisplay;
